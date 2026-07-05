d6d49c0839d2db76a70273127df296c4b2f016d4
md-samrat <md.samrat61790@gmail.com>
2026-07-05 11:48:31 +0600

    crud

--- hello_prisma/script.ts
+++ hello_prisma/script.ts
@@ -1,6 +1,8 @@
 import { prisma } from "./lib/prisma";
 
 const main = async () => {
+
+  // insert new user
   const user = await prisma.user.create({
     data: {
       name: "Alvaro",
@@ -8,6 +10,28 @@ const main = async () => {
     },
   });
   console.log("Created user:", user);
+
+
+  // get all users
+  const getUsers = await prisma.user.findMany();
+  console.log("All users:", getUsers);
+
+  // update user
+
+  const updateUser = await prisma.user.update({
+    where:{id:1},
+    data:{
+      name:"Samay Rayna",
+      email:"samay@gmail.com"
+    }
+  })
+  console.log("Updated user:", updateUser);
+
+  // delete user
+  const deleteUser = await prisma.user.delete({
+    where: {id:4},
+  })
+  console.log("Deleted user:", deleteUser);
 };
 
 main()
